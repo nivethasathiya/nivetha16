d920ea5f49014bbff96b224492c50673a78fdd03
nivethasathiya <53109733+nivethasathiya@users.noreply.github.com>
2019-08-13 16:23:38 +0530

    Create nivetha15.py

---
+++ nivetha15.py
@@ -0,0 +1,9 @@
+a = input(int("Please Enter First Number of an A.P Series: : "))
+n = intput(int("Please Enter the Total Numbers in this A.P Series: : "))
+d = intput(int("Please Enter the Common Difference : "))
+
+total = (n * (2 * a + (n - 1) * d)) / 2
+total 1 = a + (n - 1) * d
+
+print("\nThe Sum of Arithmetic Progression Series = " , total)
+print("The tn Term of Arithmetic Progression Series = " , total 1)
